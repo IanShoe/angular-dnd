1990e2e1cb5398de3bd2303385a339ef4957b7f3
Ian Shoemaker <iss90g@gmail.com>
2013-11-13 10:30:23 -0500

    no more rootscope broadcast dependancy.

--- dnd.js
+++ dnd.js
@@ -1,5 +1,5 @@
 angular.module('DragModule', []).
-service('DragService', function($rootScope){
+service('DragService', function(){
 	var currentDrag = {
 		dragging: false,
 		type: null
@@ -8,48 +8,40 @@ service('DragService', function($rootScope){
 	var dragService = {
 		setDragging : function(value, type){
 			currentDrag.dragging = value;
-			if(value){
-				currentDrag.type = type;
-			}
-			$rootScope.$broadcast('DragService.dragging', currentDrag);
-			currentDrag.type = type;
+			currentDrag.type = type || currentDrag.type;
 		},
-		getDragging : function(){
-			return currentDrag.dragging;
+		getCurrentDrag : function(){
+			return currentDrag;
 		}
 	};
 	return dragService;
 }).
-directive('ngDraggable', function (DragService) {
+directive('ngDraggable', ['DragService', function (DragService) {
 	return {
 		link: function(scope, element, attrs, ctrl) {
 			attrs.$set('draggable', true);
 			var data = scope.$eval(attrs.ngModel);
 			element.on('dragstart', function(event){
 				event.originalEvent.dataTransfer.setData("Data", JSON.stringify(data));
-				DragService.setDragging(true, data.type);
+				scope.$apply(DragService.setDragging(true, data.type));
 			});
 			element.on('dragend', function(event){
-				DragService.setDragging(false);
+				scope.$apply(DragService.setDragging(false));
 			});
 		}
 	}
-}).
-directive('ngDroppable', function (DragService) {
-	// TODO: make this so that it can be outside DragService usable (for instance, dropping files as an upload)
+}]).
+directive('ngDroppable', ['DragService', function (DragService) {
+	// TODO: make this directive triggerable outside of DragService (for instance, dropping files as an upload)
 	return {
 		link: function(scope, element, attrs) {
 			var cb = scope[attrs.ngDroppable];
 			var mediaTypes = scope.$eval(attrs.mediaTypes);
-			scope.$on('DragService.dragging', function(event, currentDrag){
+			scope.currentDrag = DragService.getCurrentDrag();
+			scope.$watch('currentDrag.dragging', function(newValue){
 				// Determine if it's a type we care about
-				if(!(mediaTypes && !contains(mediaTypes, currentDrag.type))){
-					if(currentDrag.dragging){
-						dragging();
-					}
-					else{
-						notDragging();
-					}
+				if(!(mediaTypes && !contains(mediaTypes, scope.currentDrag.type))){
+					scope.currentDrag.dragging ? dragging() : notDragging();
 				}
 			});
 
@@ -98,4 +90,4 @@ directive('ngDroppable', function (DragService) {
 			}
 		}
 	}
-});
+}]);
